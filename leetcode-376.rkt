f28795219df7787431a0a15698770b9b74af553a
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-03 09:46:33 -0400

    Solution that works, but needs memoization.

--- leetcode-376.rkt
+++ leetcode-376.rkt
@@ -3,25 +3,9 @@
 
 #lang racket
 
-; replace define with a memoized version
-(define-syntax define-memoized
-  (syntax-rules ()
-    [(_ (f args ...) bodies ...)
-     (define f
-       ; store the cache as a hash of args => result
-       (let ([results (make-hash)])
-         ; need to do this to capture both the names and the values
-         (lambda (args ...)
-           ((lambda vals
-              ; if we haven't calculated it before, do so now
-              (when (not (hash-has-key? results vals))
-                (hash-set! results vals (begin bodies ...)))
-              ; return the cached result
-              (hash-ref results vals))
-            args ...))))]))
 
 ;; Start with the first number in nums.
-(define-memoize (start-with-first nums)
+(define (start-with-first nums)
   (if (empty? nums)
       0
       (+ 1
@@ -30,7 +14,7 @@
           (choose-next-smaller (first nums) (rest nums))))))
 
 ;; Choose a number larger than curr.
-(define-memoize (choose-next-larger curr nums)
+(define (choose-next-larger curr nums)
   (cond [(empty? nums) 0]
         [(< curr (first nums))
          (max (+ 1 (choose-next-smaller (first nums) (rest nums)))
@@ -38,7 +22,7 @@
         [else (choose-next-larger curr (rest nums))]))
 
 ;; Choose a number smaller than curr.
-(define-memoize (choose-next-smaller curr nums)
+(define (choose-next-smaller curr nums)
   (cond [(empty? nums) 0]
         [(> curr (first nums))
          (max (+ 1 (choose-next-larger (first nums) (rest nums)))
