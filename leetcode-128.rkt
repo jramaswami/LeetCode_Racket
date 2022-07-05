f07eaf7c86a1e56d48fa265047485c482304e4bf
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-05 14:59:30 -0400

    TLE

---
+++ leetcode-128.rkt
@@ -0,0 +1,39 @@
+;; LeetCode :: July 2022 Challenge :: 128. Longest Consecutive Sequence
+;; jramaswami
+
+#lang racket
+
+(define/contract (longest-consecutive nums)
+  (-> (listof exact-integer?) exact-integer?)
+  ;; Turn nums into a set with O(1) access(?)
+  (define nums-set (list->set nums))
+
+  ;; Return #t if n starts sequence. It starts a sequence if
+  ;; n - 1 is not in the set of numbers.
+  (define (starts-sequence n)
+    (not (set-member? nums-set (- n 1))))
+
+  ;; Compute the length of sequence starting with n.
+  (define (sequence-length n)
+    (if (set-member? nums-set n)
+        (+ 1 (sequence-length (+ 1 n)))
+        0))
+
+  ;; Return the longest sequence that can be found.
+  (define (solve nums0)
+    (cond [(empty? nums0) 0]
+          [(starts-sequence (first nums0))
+           (max (sequence-length (first nums0)) (solve (rest nums0)))]
+          [else (solve (rest nums0))]))
+
+  (solve nums))
+    
+
+(module+ test
+  (require rackunit)
+  (let ([nums (list 100 4 200 1 3 2)]
+        [expected 4])
+    (check-equal? (longest-consecutive nums) expected))
+  (let ([nums (list 0 3 7 2 5 8 4 6 0 1)]
+        [expected 9])
+    (check-equal? (longest-consecutive nums) expected)))
