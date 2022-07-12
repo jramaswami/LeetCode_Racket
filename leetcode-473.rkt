9e84eb114d0dabefd3756d7647d86ae9efe8ce55
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-11 22:46:32 -0400

    Still TLE.

--- leetcode-473.rkt
+++ leetcode-473.rkt
@@ -10,17 +10,23 @@
 
   (define S (foldr + 0 matchsticks))
   (define T (/ S 4))
+
+  (define cache (make-hash))
   
   (define (solve matchsticks0 A B C D)
-    (cond [(or (> A T) (> B T) (> C T) (> D T)) #f]
-          [(empty? matchsticks0) #t]
-          [else (let ([hd (first matchsticks0)]
-                      [tl (rest matchsticks0)])
-                  (or
-                   (solve tl (+ A hd) B C D)
-                   (solve tl A (+ B hd) C D)
-                   (solve tl A B (+ C hd) D)
-                   (solve tl A B C (+ D hd))))]))
+    (define key (list matchsticks0 A B C D))
+    (when (not (hash-has-key? cache key))
+      (hash-set! cache key
+                 (cond [(or (> A T) (> B T) (> C T) (> D T)) #f]
+                       [(empty? matchsticks0) #t]
+                       [else (let ([hd (first matchsticks0)]
+                                   [tl (rest matchsticks0)])
+                               (or
+                                (solve tl (+ A hd) B C D)
+                                (solve tl A (+ B hd) C D)
+                                (solve tl A B (+ C hd) D)
+                                (solve tl A B C (+ D hd))))])))
+    (hash-ref cache key))
 
   (if (divides? 4 S)
       (solve matchsticks 0 0 0 0)
@@ -34,4 +40,15 @@
     (check-equal? (makesquare matchsticks) expected))
   (let ([matchsticks (list 3 3 3 3 4)]
         [expected #f])
-    (check-equal? (makesquare matchsticks) expected)))
+    (check-equal? (makesquare matchsticks) expected))
+  ;; TLE
+  (let ([matchsticks (list 12 8 12 16 20 24 28 32 36 40 44 48 52 56 60)]
+        [expected #f])
+    (check-equal? (makesquare matchsticks) expected))
+  ;; TLE
+  (let ([matchsticks (list 5969561 8742425 2513572 3352059 9084275 2194427
+                           1017540 2324577 6810719 8936380 7868365 2755770
+                           9954463 9912280 4713511)]
+        [expected #f])
+    (check-equal? (makesquare matchsticks) expected))
+  )
