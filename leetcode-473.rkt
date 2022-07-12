1368f4f194cea8c2c7a6928886b42ac9fe186c89
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-12 12:30:37 -0400

    WA

--- leetcode-473.rkt
+++ leetcode-473.rkt
@@ -5,31 +5,34 @@
 
 (require math/number-theory)
 
+;; Given a bitmaks, return #t if the selected matchsticks
+;; sum to side-length.
+(define (can-form-side? bitmask side-length matchsticks)
+  (= side-length
+     (for/sum ([b (range 15)]
+               [m matchsticks])
+       (if (bitwise-bit-set? bitmask b)
+           m
+           0))))
+
+(define (possible-bitmasks side-length matchsticks)
+  (filter
+   (lambda (bitmask) (can-form-side? bitmask side-length matchsticks))
+   (range (expt 2 (length matchsticks)))))
+
+(define (can-form-square? possible-bitmasks matchsticks-length)
+  (if (< (length possible-bitmasks) 4)
+      #f
+      (let ([ALL (- (expt 2 matchsticks-length) 1)])
+        (for/and ([combo (combinations possible-bitmasks 4)])
+          (= (apply bitwise-ior combo) ALL)))))
+        
 (define/contract (makesquare matchsticks)
   (-> (listof exact-integer?) boolean?)
-
-  (define S (foldr + 0 matchsticks))
-  (define T (/ S 4))
-
-  (define cache (make-hash))
-  
-  (define (solve matchsticks0 A B C D)
-    (define key (list matchsticks0 A B C D))
-    (when (not (hash-has-key? cache key))
-      (hash-set! cache key
-                 (cond [(or (> A T) (> B T) (> C T) (> D T)) #f]
-                       [(empty? matchsticks0) #t]
-                       [else (let ([hd (first matchsticks0)]
-                                   [tl (rest matchsticks0)])
-                               (or
-                                (solve tl (+ A hd) B C D)
-                                (solve tl A (+ B hd) C D)
-                                (solve tl A B (+ C hd) D)
-                                (solve tl A B C (+ D hd))))])))
-    (hash-ref cache key))
-
-  (if (divides? 4 S)
-      (solve matchsticks 0 0 0 0)
+  (define N (length matchsticks))
+  (define total-length (foldr + 0 matchsticks))
+  (if (divides? 4 total-length)
+      (can-form-square? (possible-bitmasks (/ total-length 4) matchsticks) N)
       #f)
   )
 
@@ -50,5 +53,9 @@
                            1017540 2324577 6810719 8936380 7868365 2755770
                            9954463 9912280 4713511)]
         [expected #f])
+    (check-equal? (makesquare matchsticks) expected))
+  ;; WA
+  (let ([matchsticks (list 5 5 5 5 4 4 4 4 3 3 3 3)]
+        [expected #t])
     (check-equal? (makesquare matchsticks) expected))
   )
